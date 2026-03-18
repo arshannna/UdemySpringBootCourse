fed5fc99273184bd6e8666114a6ad225df1cf7f6
arshanaacharya <arshanaacharya@localhost>
2026-03-18 14:03:12 -0500

    Session 8: 04-02 (half)

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -1,11 +1,29 @@
 package com.in28minutes.learn_spring_framework.examples.a1;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+// {}
+
+@Component
+class YourBuisnessClass{
 	
+	
+	
+}
+@Component
+class Dependency1{
+	
+	
+}
+@Component
+class Dependency2{
+	
+	
+	
+}
 
 @Configuration
 @ComponentScan //Automatically does component scan on the current package 
@@ -19,11 +37,10 @@ public class DependencyInjectionLauncherApplication {
 				new AnnotationConfigApplicationContext(
 						DependencyInjectionLauncherApplication.class);){
 	
-			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);;
+			//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);;
 		}
 		
 	}
 	
 	
-
 }
