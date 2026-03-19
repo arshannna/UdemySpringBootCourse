d4c0f4ca7d956aa391120d48ff294743c20dfc2f
arshanaacharya <arshanaacharya@localhost>
2026-03-19 12:30:45 -0500

    Session 8: Step 04-02

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -1,5 +1,6 @@
 package com.in28minutes.learn_spring_framework.examples.a1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +8,21 @@ import org.springframework.stereotype.Component;
 
 // {}
 
+
+
 @Component
 class YourBuisnessClass{
 	
+	@Autowired //If this is not added, it will not auto wire it -> spring automatically does field injection when @Autowired is added
+	Dependency1 dependency1;
 	
+	@Autowired 
+	Dependency2 dependency2;
+	
+	public String toString(){
+		
+		return "Using " + dependency1 + " and " + dependency2;
+	}
 	
 }
 @Component
@@ -26,7 +38,7 @@ class Dependency2{
 }
 
 @Configuration
-@ComponentScan //Automatically does component scan on the current package 
+@ComponentScan //Automatically does component scan on the current package, you can add ("package name") to specify which package to scan
 public class DependencyInjectionLauncherApplication {
 
 
@@ -38,6 +50,8 @@ public class DependencyInjectionLauncherApplication {
 						DependencyInjectionLauncherApplication.class);){
 	
 			//Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);;
+			
+			System.out.println(context.getBean(YourBuisnessClass.class)); //Better practice to use class/type of bean rather than the actual name of bean
 		}
 		
 	}
