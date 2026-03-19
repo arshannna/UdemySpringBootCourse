3006585e5bd9fd20606967223d6974f97123475d
arshanaacharya <arshanaacharya@localhost>
2026-03-19 12:44:38 -0500

    Session 8: Step 04 - 03

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectionLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -13,26 +13,57 @@ import org.springframework.stereotype.Component;
 @Component
 class YourBuisnessClass{
 	
-	@Autowired //If this is not added, it will not auto wire it -> spring automatically does field injection when @Autowired is added
+	
+	
+	//Example 1
+	//@Autowired //If this is not added, it will not auto wire it -> spring automatically does field injection when @Autowired is added
 	Dependency1 dependency1;
 	
-	@Autowired 
+	//@Autowired 
 	Dependency2 dependency2;
 	
+	
+	//Example 3
+	//@Autowired //is not needed for constructor injection
+	//Most recommended by Spring team - because all the dependencies are set when the object is created.
+	public YourBuisnessClass(Dependency1 dependency1, Dependency2 dependency2) {
+		
+		super();
+		System.out.println("Constructor Injection - YourBuisnessClass");
+		this.dependency1 = dependency1;
+		this.dependency2 = dependency2;
+	}
+
+//	//Example 2
+//	@Autowired
+//	public void setDependency1(Dependency1 dependency1) {
+//		
+//		//System.out.println("Setter Injection - setDependecy1");
+//		this.dependency1 = dependency1;
+//	}
+//
+//	@Autowired
+//	public void setDependency2(Dependency2 dependency2) {
+//		
+//		//System.out.println("Setter Injection - setDependecy2");
+//		this.dependency2 = dependency2;
+//	}
+
 	public String toString(){
 		
 		return "Using " + dependency1 + " and " + dependency2;
 	}
 	
+	
+	
 }
 @Component
 class Dependency1{
 	
-	
+
 }
 @Component
 class Dependency2{
-	
 	
 	
 }
